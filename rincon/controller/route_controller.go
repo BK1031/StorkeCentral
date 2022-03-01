1be0622f9fa5d355b86879decad973a0847a9bef
Bharat Kathi <bharat1031@gmail.com>
2022-03-01 15:47:15 -0800

    Get all status endpoint

--- rincon/controller/route_controller.go
+++ rincon/controller/route_controller.go
@@ -7,5 +7,6 @@ func InitializeRoutes(router *gin.Engine)  {
 	router.GET("/services", GetAllServices)
 	router.GET("/services/:name", GetService)
 	router.POST("/services", CreateService)
+	router.GET("/status", GetAllServiceStatus)
 	router.GET("/status/:name", GetServiceStatus)
 }
--- rincon/controller/status_controller.go
+++ rincon/controller/status_controller.go
@@ -6,6 +6,20 @@ import (
 	"rincon/service"
 )
 
+func GetAllServiceStatus(c *gin.Context)  {
+	returnList := []gin.H{}
+	services := service.GetAllServices()
+	if len(services) == 0 {
+		c.JSON(http.StatusNotFound, returnList)
+		return
+	}
+	for i, s := range services {
+		println(i, s.Name)
+		returnList = append(returnList, service.GetServiceStatus(s))
+	}
+	c.JSON(http.StatusOK, returnList)
+}
+
 func GetServiceStatus(c *gin.Context) {
 	returnList := []gin.H{}
 	services := service.GetServiceByName(c.Param("name"))
