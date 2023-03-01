8b53deb6614cd428950c52661c08a062eb31bc05
Bharat Kathi <bharat1031@gmail.com>
2023-03-01 02:17:48 -0800

    setup onesignal and subscribed user to os

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Bharat Kathi on 3/1/23.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
