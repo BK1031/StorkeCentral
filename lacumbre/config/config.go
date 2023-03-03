324f956b07fe03094cbf71d3768227052fbbaafc
Bharat Kathi <bharat1031@gmail.com>
2023-03-02 21:26:57 -0800

    fixed miranda url in lacumbre

--- lacumbre/config/config.go
+++ lacumbre/config/config.go
@@ -2,7 +2,7 @@ package config
 
 import "os"
 
-var Version = "2.3.1"
+var Version = "2.3.2"
 var Env = os.Getenv("ENV")
 var Port = os.Getenv("PORT")
 var RinconPort = os.Getenv("RINCON_PORT")
--- lacumbre/service/miranda_service.go
+++ lacumbre/service/miranda_service.go
@@ -8,9 +8,9 @@ import (
 func SendMirandaNotification(mirandaBody []byte) {
 	// TODO: Make this actually get the correct miranda dns value
 	responseBody := bytes.NewBuffer(mirandaBody)
-	_, err := http.Post("miranda"+":"+"4007"+"/notifications", "application/json", responseBody)
+	_, err := http.Post("http://miranda"+":"+"4007"+"/notifications", "application/json", responseBody)
 	if err != nil {
-		println("Error sending notification :(")
+		println("Error sending notification :(" + err.Error())
 	} else {
 		println("Sent notification to Miranda!")
 	}
