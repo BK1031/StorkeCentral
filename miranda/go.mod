1d06b4692b256f1b506fef4f483e69725e4d3e28
Bharat Kathi <bharat1031@gmail.com>
2023-05-03 14:58:32 -0700

    added discord logging

--- miranda/go.mod
+++ miranda/go.mod
@@ -1,6 +1,6 @@
 module miranda
 
-go 1.19
+go 1.20
 
 require (
 	firebase.google.com/go/v4 v4.10.0
--- miranda/service/onesignal_service.go
+++ miranda/service/onesignal_service.go
@@ -25,8 +25,10 @@ func CreateOSNotification(notification *onesignal.Notification) {
 	resp, r, err := OneSignal.DefaultApi.CreateNotification(appAuth).Notification(*notification).Execute()
 
 	if err != nil {
+		_, _ = Discord.ChannelMessageSend(config.DiscordChannel, ":rotating_light: Failed to send OneSignal notification! Check logs for more info.")
 		fmt.Fprintf(os.Stderr, "Error when calling `DefaultApi.CreateNotification``: %v\n", err)
 		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
 	}
+	_, _ = Discord.ChannelMessageSend(config.DiscordChannel, "Successfully sent OneSignal notification: "+resp.Id)
 	fmt.Fprintf(os.Stdout, "Response from `DefaultApi.CreateNotification`: %v\n", resp)
 }
