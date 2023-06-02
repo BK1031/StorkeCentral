c2bd723653e8b0b64bc5bf44b4fbb4b07f829fcb
Bharat Kathi <bharat1031@gmail.com>
2023-06-01 19:09:23 -0700

    remove existing passtime before updating so createdAt field gets updated

--- tepusquet/config/config.go
+++ tepusquet/config/config.go
@@ -2,7 +2,7 @@ package config
 
 import "os"
 
-var Version = "1.6.4"
+var Version = "1.6.5"
 var Env = os.Getenv("ENV")
 var Port = os.Getenv("PORT")
 var RinconPort = os.Getenv("RINCON_PORT")
--- tepusquet/service/passtime_service.go
+++ tepusquet/service/passtime_service.go
@@ -17,13 +17,10 @@ func GetPasstimeForUserForQuarter(userID string, quarter string) model.UserPasst
 }
 
 func CreatePasstimeForUser(passtime model.UserPasstime) error {
-	if DB.Where("user_id = ? AND quarter = ?", passtime.UserID, passtime.Quarter).Updates(&passtime).RowsAffected == 0 {
-		println("New passtime for user " + passtime.UserID + " for quarter " + passtime.Quarter)
-		if result := DB.Create(&passtime); result.Error != nil {
-			return result.Error
-		}
-	} else {
-		println("Passtime for user " + passtime.UserID + " for quarter " + passtime.Quarter + " has been updated!")
+	DB.Where("user_id = ? AND quarter = ?", passtime.UserID, passtime.Quarter).Delete(&model.UserPasstime{})
+	println("New passtime for user " + passtime.UserID + " for quarter " + passtime.Quarter)
+	if result := DB.Create(&passtime); result.Error != nil {
+		return result.Error
 	}
 	return nil
 }
