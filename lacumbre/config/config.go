772e75feccd1902e1ce5fc74dc0d01d7f02ffe6e
Bharat Kathi <bharat1031@gmail.com>
2023-08-14 12:58:18 -0700

    updated firebase service to zap logging

--- lacumbre/config/config.go
+++ lacumbre/config/config.go
@@ -7,7 +7,7 @@ import (
 
 var Service = model.Service{}
 
-var Version = "2.3.4"
+var Version = "2.3.6"
 var Env = os.Getenv("ENV")
 var Port = os.Getenv("PORT")
 var RinconPort = os.Getenv("RINCON_PORT")
--- lacumbre/service/firebase_service.go
+++ lacumbre/service/firebase_service.go
@@ -6,7 +6,7 @@ import (
 	firebase "firebase.google.com/go/v4"
 	"google.golang.org/api/option"
 	"lacumbre/config"
-	"log"
+	"lacumbre/utils"
 	"time"
 )
 
@@ -15,7 +15,7 @@ var FirebaseAdmin *firebase.App
 func InitializeFirebase() {
 	decoded, err := base64.StdEncoding.DecodeString(config.FirebaseServiceAccountEncoded)
 	if err != nil {
-		log.Fatalf("Error decoding service account: %v\n", err)
+		utils.SugarLogger.Fatalln("Error decoding service account: %v\n", err)
 	}
 	ctx := context.Background()
 	conf := &firebase.Config{
@@ -25,7 +25,7 @@ func InitializeFirebase() {
 	opt := option.WithCredentialsJSON(decoded)
 	app, err := firebase.NewApp(ctx, conf, opt)
 	if err != nil {
-		log.Fatalln("Error initializing app:", err)
+		utils.SugarLogger.Fatalln("Error initializing app:", err)
 	}
 	FirebaseAdmin = app
 	FirebaseDBTest()
@@ -35,7 +35,7 @@ func FirebaseDBTest() {
 	ctx := context.Background()
 	client, err := FirebaseAdmin.Firestore(ctx)
 	if err != nil {
-		log.Printf("An error has occurred: %s", err)
+		utils.SugarLogger.Fatalln("An error has occurred: %s", err)
 	}
 	client.Collection("testing").Add(ctx, map[string]interface{}{
 		"message":   config.Service.Name + " v" + config.Version + " is online!",
