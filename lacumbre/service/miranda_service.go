9e2cfc094a98b43e736247ca71f8a362c8d603f7
Bharat Kathi <bharat1031@gmail.com>
2023-09-13 10:35:29 -0700

    lacumbre add miranda notification struct

--- lacumbre/service/miranda_service.go
+++ lacumbre/service/miranda_service.go
@@ -1,18 +1,21 @@
 package service
 
-import (
-	"bytes"
-	"lacumbre/utils"
-	"net/http"
-)
+type MirandaNotification struct {
+	ID         string        `json:"id"`
+	UserID     string        `json:"user_id"`
+	Sender     string        `json:"sender"`
+	Title      string        `json:"title"`
+	Body       string        `json:"body"`
+	PictureUrl string        `json:"picture_url"`
+	LaunchUrl  string        `json:"launch_url"`
+	Route      string        `json:"route"`
+	Priority   string        `json:"priority"`
+	Push       bool          `json:"push"`
+	Read       bool          `json:"read"`
+	Data       []interface{} `json:"data"`
+}
 
-func SendMirandaNotification(mirandaBody []byte) {
+func SendMirandaNotification(mirandaBody MirandaNotification) {
 	// TODO: Make this actually get the correct miranda dns value
-	responseBody := bytes.NewBuffer(mirandaBody)
-	_, err := http.Post("http://miranda"+":"+"4007"+"/notifications", "application/json", responseBody)
-	if err != nil {
-		utils.SugarLogger.Errorln("Error sending notification :(" + err.Error())
-	} else {
-		utils.SugarLogger.Infoln("Sent notification to Miranda!")
-	}
+
 }
