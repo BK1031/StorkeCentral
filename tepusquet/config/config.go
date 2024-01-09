80f6fc4c0ec69e60f335aae244fa93742e3bb2fe
Bharat Kathi <bharat1031@gmail.com>
2024-01-08 22:16:34 -0800

    version bump

--- tepusquet/config/config.go
+++ tepusquet/config/config.go
@@ -13,7 +13,7 @@ var Service = model.Service{
 	Version:     Version,
 }
 
-var Version = "2.3.0"
+var Version = "2.3.1"
 var Env = os.Getenv("ENV")
 var Port = os.Getenv("PORT")
 var RinconPort = os.Getenv("RINCON_PORT")
